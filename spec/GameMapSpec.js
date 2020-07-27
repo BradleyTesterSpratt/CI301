f985c1d9d2b12c33b1bb5b55a27d5ade7d973212
BradleyTesterSpratt <bradley.tester-spratt@bbc.co.uk>
2020-07-27 22:16:15 +0100

    add tests for game maps
    add comments for map and terrain changes for player visibility

---
+++ spec/GameMapSpec.js
@@ -0,0 +1,36 @@
+describe("GameMap", function() {
+  var GameMap = require('../lib/GameMap');
+  var Terrain = require('../lib/Terrain');
+  var map;
+  var terrain;
+
+  describe("setTerrain", function() {
+    beforeEach(function() {
+      map = new GameMap();
+      terrain = new Terrain({x:2, y:2});
+    });
+
+    it("should add the new terrain to the terrainList" , function() {
+      map.setTerrain(terrain);
+      expect(map.terrainList).toContain(terrain);
+    });
+  });
+
+  describe("getTerrainByPosition", function() {
+    beforeEach(function() {
+      map = new GameMap();
+      terrain = new Terrain({x:2, y:2});
+    });
+
+    it("should find the terrain if there is an terrain at that position in the terrainList" , function() {
+      map.setTerrain(terrain);
+      expect(map.getTerrainByPosition({x:2, y:2})).toBe(terrain);
+    });
+
+    it("should not find a terrain if there is not terrain at that position in the terrainList" , function() {
+      map.setTerrain(terrain);
+      expect(map.getTerrainByPosition({x:2, y:1})).not.toBe(terrain);
+    });
+  });
+
+});

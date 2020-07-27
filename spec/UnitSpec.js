781163325e63890bcd69827dfd0b09d39512c7e6
BradleyTesterSpratt <bradley.tester-spratt@bbc.co.uk>
2020-07-27 18:57:27 +0100

    add map starts and some tests

--- spec/UnitSpec.js
+++ spec/UnitSpec.js
@@ -1,5 +1,8 @@
 describe("Unit", function() {
   var Unit = require('../lib/Unit');
+  var GameMap = require('../lib/GameMap');
+  var Terrain = require('../lib/Terrain');
+  var map;
   var unit;
   var enemy;
   var units;
@@ -14,12 +17,14 @@ describe("Unit", function() {
   describe("setDestination", function() {
 
     beforeEach(function() {
-      unit = new Unit("idle", {x:0, y:0});
+      map = new GameMap();
+      unit = new Unit("idle", {x:0, y:0}, map);
+      units = [unit];
     });
     
     it("should be able to move if state is not dead", function() {
       unit.setDestination({x:1, y:1});
-      unit.performAction();
+      updateAll();
       expect(unit.position).toEqual({x:1, y:1});
       expect(unit.state).toEqual("idle");
     });
@@ -27,40 +32,43 @@ describe("Unit", function() {
     it("should not be able to move if state is dead", function() {
       unit.setState("dead");
       unit.setDestination({x:1, y:1});
-      unit.performAction();
+      updateAll();
       expect(unit.position).toEqual({x:0, y:0});
     });
 
     it("should not reach a distant destination in a single movement", function() {
       unit.setDestination({x:3, y:1});
-      unit.performAction();
+      updateAll();
       expect(unit.position).not.toEqual({x:0, y:0});
       expect(unit.position).not.toEqual({x:3, y:1});
       expect(unit.state).toEqual("moving");
-      unit.performAction();
+      updateAll();
       expect(unit.position).toEqual({x:3, y:1});
       expect(unit.state).toBe("idle");
     });
 
-    // it("should not be able to traverse an impassible space", function() {
-    //   unit.setDestination({x:-1, y:-1});
-    //   unit.performAction();
-    // });
+    it("should not be able to traverse an impassible space", function() {
+      map.setTerrain(new Terrain({x:-1, y:-1}));
+      unit.setDestination({x:-1, y:-1});
+      updateAll();
+      expect(unit.position).not.toEqual({x:-1, y:-1});
+    });
   });
 
   describe("attack", function() {
 
     beforeEach(function() {
-      unit = new Unit("idle", {x:0, y:0});
-      enemy = new Unit("idle", {x:1, y:0});
+      map = new GameMap();
+      unit = new Unit("idle", {x:0, y:0}, map);
+      enemy = new Unit("idle", {x:1, y:0}, map);
       units = [unit, enemy];
     });
 
     it("should attack an adjacent enemy succesfully", function() {
       unit.setTarget(enemy);
-      unit.performAction();
+      updateAll();
       expect(unit.state).toBe("attacking");
-      unit.performAction();
+      updateAll();
       expect(unit.state).toBe("attacking");
       expect(enemy).toBeDamaged();
       expect(enemy.state).toBe("underAttack");
@@ -69,7 +77,7 @@ describe("Unit", function() {
     it("should move closer to the enemy if it is not adjacent", function() {
       enemy.position = ({x:2, y:0});
       unit.setTarget(enemy);
-      unit.performAction();
+      updateAll();
       expect(enemy).not.toBeDamaged();
       expect(unit.state).toBe("closingDistance");
       expect(enemy.state).toBe("idle");
@@ -78,17 +86,17 @@ describe("Unit", function() {
     it("should attack the enemy once in range", function() {
       enemy.position = ({x:2, y:0});
       unit.setTarget(enemy);
-      unit.performAction();
+      updateAll();
       expect(unit.state).toBe("closingDistance");
       expect(enemy).not.toBeDamaged();
-      unit.performAction();
+      updateAll();
       expect(enemy).not.toBeDamaged();
       expect(unit.state).toBe("attacking");
-      unit.performAction();
+      updateAll();
       expect(enemy.state).toBe("idle");
       expect(enemy).not.toBeDamaged();
       expect(unit.state).toBe("attacking");
-      unit.performAction();
+      updateAll();
       expect(enemy).toBeDamaged();
       expect(enemy.state).toBe("underAttack");
     });
@@ -115,9 +123,15 @@ describe("Unit", function() {
       expect(unit).toBeInRange();
     });
     
-    // it("should not cause the unit to overlap the enemy", function() {
-
-    // });
+    it("should not cause the unit to overlap the enemy", function() {
+      enemy.position = {x:2, y:0};
+      unit.setTarget(enemy);
+      expect(unit).not.toBeInRange();
+      updateAll();
+      updateAll();
+      expect(unit).toBeInRange();
+      expect(unit.position).not.toEqual(enemy.position);
+    });
   });
 
 });
